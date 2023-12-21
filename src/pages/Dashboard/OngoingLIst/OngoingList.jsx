530cfdb2211c4c9cbaf599ec4bc124d30c38ad88
Anirbandasjoy <joy600508@gmail.com>
2023-12-22 02:55:07 +0600

    added chart my dashboard

--- src/pages/Dashboard/OngoingLIst/OngoingList.jsx
+++ src/pages/Dashboard/OngoingLIst/OngoingList.jsx
@@ -1,9 +1,38 @@
+import { MdOutlineDateRange } from "react-icons/md";
 import Title from "../../../components/Title/Title";
+import useFetchTask from "../../../hooks/useFetchTask";
+import { RxUpdate } from "react-icons/rx";
 
 const OngoingList = () => {
+  const { data } = useFetchTask("ongoing");
   return (
     <div>
       <Title title="Ongoing List" />
+      <div className="mt-8 spy flex flex-col gap-3">
+        {data?.map((task) => {
+          return (
+            <div
+              key={task?._id}
+              className="bg-blue-100 p-4 border cursor-pointer border-blue-200"
+            >
+              <h1 className="text-2xl font-semibold text-gray-700">
+                {task?.title}
+              </h1>
+              <p className="mt-2">{task?.description}</p>
+              <div className="mt-4 flex items-center gap-4">
+                <div className="flex  items-center gap-2">
+                  <MdOutlineDateRange />
+                  <h2>{task?.deadline}</h2>
+                </div>
+                <div className="flex items-center gap-2">
+                  <RxUpdate />
+                  <h2>{task?.priority}</h2>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
--- src/pages/Dashboard/TodoList/TodoList.jsx
+++ src/pages/Dashboard/TodoList/TodoList.jsx
@@ -8,27 +8,30 @@ const TodoList = () => {
   return (
     <div>
       <Title title="Todo List" />
-      <div className="mt-8 spy">
-        <div className="bg-blue-200 p-4 border  border-blue-400">
-          <h1 className="text-2xl font-semibold text-gray-700">
-            hello i am job task title
-          </h1>
-          <p className="mt-2">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut
-            perferendis aliquam aperiam, beatae reprehenderit quisquam
-            voluptates delectus! Numquam, aut! Similique.
-          </p>
-          <div className="mt-4 flex items-center gap-4">
-            <div className="flex  items-center gap-2">
-              <MdOutlineDateRange />
-              <h2>12/21/2023</h2>
+      <div className="mt-8 spy flex flex-col gap-3">
+        {data?.map((task) => {
+          return (
+            <div
+              key={task?._id}
+              className="bg-blue-100 p-4 border cursor-pointer border-blue-200"
+            >
+              <h1 className="text-2xl font-semibold text-gray-700">
+                {task?.title}
+              </h1>
+              <p className="mt-2">{task?.description}</p>
+              <div className="mt-4 flex items-center gap-4">
+                <div className="flex  items-center gap-2">
+                  <MdOutlineDateRange />
+                  <h2>{task?.deadline}</h2>
+                </div>
+                <div className="flex items-center gap-2">
+                  <RxUpdate />
+                  <h2>{task?.priority}</h2>
+                </div>
+              </div>
             </div>
-            <div className="flex items-center gap-2">
-              <RxUpdate />
-              <h2>Low</h2>
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );

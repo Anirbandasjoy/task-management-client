bfca8c2695b3026e91c19c0371f9d1904660f97e
Anirbandasjoy <joy600508@gmail.com>
2023-12-22 04:03:28 +0600

    task delete process complete

--- src/pages/Dashboard/OngoingLIst/OngoingList.jsx
+++ src/pages/Dashboard/OngoingLIst/OngoingList.jsx
@@ -8,26 +8,38 @@ const OngoingList = () => {
   return (
     <div>
       <Title title="Ongoing List" />
-      <div className="mt-8 spy flex flex-col gap-3">
+      <div className="mt-8 spy flex flex-col gap-3 h-[calc(100vh-190px)] overflow-auto sm:overflow-visible ">
         {data?.map((task) => {
           return (
             <div
               key={task?._id}
-              className="bg-gray-100 dark:bg-gray-800 p-4 border cursor-pointer border-blue-200"
+              className="bg-gray-100  flex lg:items-center flex-col lg:flex-row lg:justify-between dark:bg-gray-800 p-4 border cursor-pointer border-blue-200"
             >
-              <h1 className="text-2xl font-semibold dark:text-gray-300 text-gray-700">
-                {task?.title}
-              </h1>
-              <p className="mt-2 dark:text-gray-300">{task?.description}</p>
-              <div className="mt-4 flex items-center gap-4">
-                <div className="flex  items-center gap-2">
-                  <MdOutlineDateRange className="dark:text-gray-300" />
-                  <h2 className="dark:text-gray-300">{task?.deadline}</h2>
+              <div>
+                <h1 className="text-2xl font-semibold text-gray-700 dark:text-gray-300">
+                  {task?.title}
+                </h1>
+                <p className="mt-2 dark:text-gray-300 text-justify">
+                  {task?.description}
+                </p>
+                <div className="mt-4 flex items-center gap-4">
+                  <div className="flex  items-center gap-2">
+                    <MdOutlineDateRange className="dark:text-gray-300" />
+                    <h2 className="dark:text-gray-300">{task?.deadline}</h2>
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <RxUpdate className="dark:text-gray-300" />
+                    <h2 className="dark:text-gray-300">{task?.priority}</h2>
+                  </div>
                 </div>
-                <div className="flex items-center gap-2">
-                  <RxUpdate className="dark:text-gray-300 dark:text-gray-300" />
-                  <h2 className="dark:text-gray-300">{task?.priority}</h2>
-                </div>
+              </div>
+              <div className="flex lg:flex-col flex-row gap-5 pl-3 mt-5 lg:mt-0 ">
+                <button className="btn btn-sm rounded-sm text-gray-200 text-xs w-16 btn-success">
+                  Delete
+                </button>
+                <button className="btn btn-sm rounded-sm text-gray-200 text-xs w-16 btn-success">
+                  Edit
+                </button>
               </div>
             </div>
           );

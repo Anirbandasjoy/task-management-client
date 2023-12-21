296b1a87328235c679f19fe6ead61d82da7e1bc9
Anirbandasjoy <joy600508@gmail.com>
2023-12-22 00:56:38 +0600

    added job task static card

--- src/pages/Dashboard/TodoList/TodoList.jsx
+++ src/pages/Dashboard/TodoList/TodoList.jsx
@@ -1,8 +1,33 @@
 import Title from "../../../components/Title/Title";
+import { MdOutlineDateRange } from "react-icons/md";
+import { RxUpdate } from "react-icons/rx";
 const TodoList = () => {
   return (
     <div>
       <Title title="Todo List" />
+
+      <div className="mt-8 spy">
+        <div className="bg-blue-200 p-4 border  border-blue-400">
+          <h1 className="text-2xl font-semibold text-gray-700">
+            hello i am job task title
+          </h1>
+          <p className="mt-2">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut
+            perferendis aliquam aperiam, beatae reprehenderit quisquam
+            voluptates delectus! Numquam, aut! Similique.
+          </p>
+          <div className="mt-4 flex items-center gap-4">
+            <div className="flex  items-center gap-2">
+              <MdOutlineDateRange />
+              <h2>12/21/2023</h2>
+            </div>
+            <div className="flex items-center gap-2">
+              <RxUpdate />
+              <h2>Low</h2>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
